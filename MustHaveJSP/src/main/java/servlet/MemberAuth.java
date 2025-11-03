8610678b8341f3a0027f6aa8abf82492cfa573ee
zodib0123 <zodib0123@gmail.com>
2025-11-03 18:24:20 +0900

    JSP (12장 서블릿 (MVC) 실습예제)

---
+++ MustHaveJSP/src/main/java/servlet/MemberAuth.java
@@ -0,0 +1,58 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import membership.MemberDAO;
+import membership.MemberDTO;
+
+public class MemberAuth extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	MemberDAO dao;
+	
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		ServletContext application = this.getServletContext();
+		
+		String driver = application.getInitParameter("MySQLDriver");
+		String connectUrl = application.getInitParameter("MySQLURL");
+		String oId = application.getInitParameter("MySQLId");
+		String oPass = application.getInitParameter("MySQLPwd");
+		
+		dao = new MemberDAO(driver, connectUrl, oId, oPass);
+	}
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String admin_id = this.getInitParameter("admin_id");
+		
+		String id = req.getParameter("id");
+		String pass = req.getParameter("pass");
+		
+		MemberDTO memberDTO = dao.getMemberDTO(id, pass);
+		
+		String memberName = memberDTO.getName();
+		if (memberName != null) {
+			req.setAttribute("authMessage", memberName + " 회원님 반갑습니다.");
+		} else {
+			if (admin_id.equals(id)) {
+				req.setAttribute("authMessage", admin_id + "는 최고 관리자입니다.");
+			} else {
+				req.setAttribute("authMessage", "귀하는 회원이 아닙니다.");
+			}
+		}
+		req.getRequestDispatcher("/12Servlet/MemberAuth.jsp").forward(req, resp);
+	}
+	
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		dao.close();
+	}
+}
